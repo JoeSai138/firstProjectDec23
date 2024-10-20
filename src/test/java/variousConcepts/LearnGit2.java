18797681645a11419cf6982786c0cfa30d43494b
SaiJoe1 <164972377+SaiJoe1@users.noreply.github.com>
2024-10-20 08:26:16 -0500

    Learning git

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,14 @@
+package variousConcepts;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("In Main method");
+		
+	}
+	
+	public void printme1() {
+		System.out.println("Git testing");
+	}
+
+}
